426302626c87e3b001797a6a491fa8c992118cdf
HappyAreaBean <bean@happyareabean.cc>
2022-08-12 17:11:32 +0800

    fix: `{player}` placeholder is not displaying the correct player name

--- src/main/java/cc/happyareabean/sjm/utils/MessageUtil.java
+++ src/main/java/cc/happyareabean/sjm/utils/MessageUtil.java
@@ -6,17 +6,19 @@ import net.kyori.adventure.audience.Audience;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MessageUtil {
 
 	public static void sendMessageDelay(Player player, final List<String> messages, int delayTicks) {
-		messages.replaceAll(s -> s.replace("{player}", player.getName()));
 		Audience aPlayer = SimpleJoinMessage.getAdventure().player(player);
 		new BukkitRunnable() {
 			@Override
 			public void run() {
-				aPlayer.sendMessage(SimpleJoinMessage.MINIMESSAGE.deserialize(String.join("\n", (SimpleJoinMessage.getInstance().isPAPISupported() ? PlaceholderAPI.setPlaceholders(player, messages) : messages))));
+				List<String> newMessage = new ArrayList<>();
+				messages.forEach((m) -> newMessage.add(m.replace("{player}", player.getName())));
+				aPlayer.sendMessage(SimpleJoinMessage.MINIMESSAGE.deserialize(String.join("\n", (SimpleJoinMessage.getInstance().isPAPISupported() ? PlaceholderAPI.setPlaceholders(player, newMessage) : newMessage))));
 			}
 		}.runTaskLater(SimpleJoinMessage.getInstance(), (delayTicks > 0 ? delayTicks : 1L));
 	}
