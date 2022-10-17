be178137cc9261202aa2901c98ea9ff7202a9239
HappyAreaBean <bean@happyareabean.cc>
2022-10-17 10:11:46 +0800

    feat: added `time`, `online` placeholder and timeFormat in config

--- src/main/java/cc/happyareabean/sjm/config/SJMConfig.java
+++ src/main/java/cc/happyareabean/sjm/config/SJMConfig.java
@@ -18,16 +18,24 @@ public class SJMConfig extends YamlConfiguration {
 			" ",
 			"Placeholders available:",
 			"   {player} - Player name",
+			"   {time} - Current time",
+			"   {online} - Server online players count",
 	})
 	private List<String> joinMessage = Arrays.asList(
 			"",
 			"<b>Hello <color:#3cff00>{player}</color>! Welcome to <color:#00d9ff>My Server</color>!</b>",
-			"Currently have <yellow>%server_online%</yellow> players online! <b><color:#54585c>|</color></b> Time: <color:#ffac05>%server_time_dd/MM/yyyy hh:mm:ss%</color>",
+			"Currently have <yellow>{online}</yellow> players online! <b><color:#54585c>|</color></b> Time: <color:#ffac05>{time}</color>",
 			"",
 			"<b><color:#5865f2><click:open_url:'https://go.happyareabean.cc/supportdiscord'><hover:show_text:'<green>Click to join our <b><color:#5865f2>Discord</color></b> server!'>[Click here to join our Discord server]</hover></click></color></b>",
 			"<b><gradient:#f7ff00:#db36a4><click:open_url:'https://go.happyareabean.cc/sjm'><hover:show_text:'Click to Download <gradient:#f7ff00:#db36a4><b>SimpleJoinMessage</b></gradient>'>[Download SimpleJoinMessage]</hover></click></gradient></b>",
 			""
 	);
+
+	@Comment({
+			"The time format will be used in the {time} placeholder.",
+			"You can see all the available patterns here: https://go.happyareabean.cc/O5GZR"
+	})
+	private String timeFormat = "dd/MM/yyyy hh:mm:ss";
 
 	@Comment({
 			"Whether the message should be delayed? (In ticks)",
--- src/main/java/cc/happyareabean/sjm/utils/MessageUtil.java
+++ src/main/java/cc/happyareabean/sjm/utils/MessageUtil.java
@@ -1,23 +1,37 @@
 package cc.happyareabean.sjm.utils;
 
 import cc.happyareabean.sjm.SimpleJoinMessage;
+import cc.happyareabean.sjm.config.SJMConfig;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.audience.Audience;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MessageUtil {
 
 	public static void sendMessageDelay(Player player, final List<String> messages, int delayTicks) {
 		Audience aPlayer = SimpleJoinMessage.getAdventure().player(player);
+		SJMConfig config = SimpleJoinMessage.getInstance().getSJMConfig();
+		DateTimeFormatter formatter =
+				DateTimeFormatter.ofPattern(config.getTimeFormat())
+						.withZone( ZoneId.systemDefault() );
 		new BukkitRunnable() {
 			@Override
 			public void run() {
 				List<String> newMessage = new ArrayList<>();
-				messages.forEach((m) -> newMessage.add(m.replace("{player}", player.getName())));
+				messages.forEach((m) -> newMessage.add(m.replace("{player}", player.getName())
+						.replace("{online}", String.valueOf(Bukkit.getServer().getOnlinePlayers().size()))
+						.replace("{time}", formatter.format(Instant.now()))));
 				aPlayer.sendMessage(SimpleJoinMessage.MINIMESSAGE.deserialize(String.join("\n", (SimpleJoinMessage.getInstance().isPAPISupported() ? PlaceholderAPI.setPlaceholders(player, newMessage) : newMessage))));
 			}
 		}.runTaskLater(SimpleJoinMessage.getInstance(), (delayTicks > 0 ? delayTicks : 1L));
